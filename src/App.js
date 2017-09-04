7c86fc0d454b799a07e46f370869b742e88ced07
George Kunthara <gkunthara1@gmail.com>
2017-09-04 14:13:46 -0700

    Update
    Fix sizing, add gradients

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import {AreaClosed} from '@vx/shape';
 import { scaleTime, scaleLinear } from '@vx/scale';
 import { extent, max } from 'd3-array';
 import {Group} from '@vx/group';
+import { GridRows, GridColumns } from '@vx/grid';
+import styled from 'styled-components';
 
 //set width, height, and margins
-const width = 750;
-const height = 400;
+const width = 1250; //1250
+const height = 650; //650
 const margin = {
     top: 60,
     bottom: 60,
@@ -61,6 +63,15 @@ class Chart extends Component {
 
     render() {
 
+        const Wrapper = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        
+    `;
+
+
         //scale x-axis
         const xScale = scaleTime({
             range: [0, xMax],
@@ -70,25 +81,91 @@ class Chart extends Component {
         //scale y-axis
         const yScale = scaleLinear({
             range: [yMax, 0],
-            domain: [0, max(this.state.dataArr, y)]
+            domain: [200, max(this.state.dataArr, y)]
         });
 
         return (
 
+
+            <Wrapper>
             <div className="Chart">
                 <svg width={width} height={height}>
+
                     <Group top={margin.top} left={margin.left}>
+
+                        <rect
+                            x={0}
+                            y={0}
+                            width={xMax}
+                            height={yMax}
+                            fill="#32deaa"
+                            rx={10}
+                        />
+
+
+                        <GridRows
+                            lineStyle={{ pointerEvents: 'none' }}
+                            scale={yScale}
+                            width={xMax}
+                            strokeDasharray="2,2"
+                            stroke="rgba(255,255,255,0.3)"
+                        />
+
+                        <GridColumns
+                            lineStyle={{ pointerEvents: 'none' }}
+                            scale={xScale}
+                            height={yMax}
+                            strokeDasharray="2,2"
+                            stroke="rgba(255,255,255,0.3)"
+                        />
+                        <linearGradient
+                            id="gradient"
+                            x1="0%"
+                            y1="0%"
+                            x2="0%"
+                            y2="100%"
+                        >
+                            <stop offset="0%" stopColor="#FFFFFF" stopOpacity={1} />
+                            <stop
+                                offset="100%"
+                                stopColor="#FFFFFF"
+                                stopOpacity={0.2}
+                            />
+                        </linearGradient>
+
                         <AreaClosed
                             data={this.state.dataArr}
                             xScale={xScale}
                             yScale={yScale}
                             x={x}
                             y={y}
-                            fill={"red"}
+                            fill={"url(#gradient)"}
+                            stroke={""}
+
                         />
+                        <AxisBottom
+                            scale={xScale}
+                            top={yMax}
+                            label={''}
+                            stroke={'#1b1a1e'}
+                            tickTextFill={'#1b1a1e'}
+                            numTicks = {7}
+                        />
+                        <AxisLeft
+                            scale={yScale}
+                            top={0}
+                            left={0}
+                            label={'Price ($)'}
+                            stroke={'#1b1a1e'}
+                            tickTextFill={'#1b1a1e'}
+                            numTicks =  {5}
+                        />
+
+
                     </Group>
                 </svg>
             </div>
+            </Wrapper>
         )
     }
 }

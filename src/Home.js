d830039467beb0609b130f0a8f8bf83850d91dfe
George Kunthara <gkunthara1@gmail.com>
2017-09-20 20:16:18 -0700

    Home cards
    Add cards on homepage to direct to specific currency

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import {Header} from './ChartHeader';
 import styled from 'styled-components';
-
+import { Card, Col, Row  } from 'antd';
+import './antd/dist/antd.css';
+import {Link} from 'react-router-dom';
 
 
 
@@ -16,14 +17,51 @@ export class Home extends Component {
         justify-content: center;
         align-items: center;
         margin-top: 25px;
-        //margin-left: 80px;
 
     `;
+
+        const pStyle ={
+            marginTop: 25,
+            marginBottom: 25
+        };
+
+        const cardStyle = {
+            width: 175,
+            height: 150
+        }
+
 
         return(
             <div>
                 <Wrapper>
-                    Welcome to Token Chart Where you can view all your favorite tokens!
+                    <div className="jumbotron text-center">
+                        <div className="container">
+                            <h1 className="display-3">Crypto Chart</h1>
+                            <p style={pStyle} className="lead">Check out prices of Ethereum & Bitcoin.</p>
+                                <Row type="flex" justify="center">
+                                    <Col span={4}>
+                                        <Link to={'/ethereum'}>
+                                            <Card style={cardStyle}>
+                                                <div className="custom-image">
+                                                    <img alt="example" width="50%" src="https://www.ethereum.org/images/logos/ETHEREUM-ICON_Black.png"/>
+                                                    <h4 style={{marginTop: 10, fontFamily: 'Roboto'}}>Ethereum</h4>
+                                                </div>
+                                            </Card>
+                                        </Link>
+                                    </Col>
+                                    <Col span={4}>
+                                        <Link to={'/bitcoin'}>
+                                            <Card style={cardStyle}>
+                                                <div className="custom-image">
+                                                    <img alt="example" width="50%" src="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png"/>
+                                                    <h4 style={{marginTop: 10, fontFamily: 'Roboto'}}>Bitcoin</h4>
+                                                </div>
+                                            </Card>
+                                        </Link>
+                                    </Col>
+                                </Row>
+                        </div>
+                    </div>
                 </Wrapper>
             </div>
         )
